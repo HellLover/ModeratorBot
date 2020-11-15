717ad80fe116ef493eaaf1314056b9bf0a40e153
HellLover <68247946+HellLover@users.noreply.github.com>
2020-11-15 16:18:14 +0400

    Update message.js

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -13,9 +13,45 @@ module.exports = class extends Event {
     }
 
     async execute(message) {
-        if (!message.content.startsWith(this.client.prefix)) return;
         if(message.author.bot) return;
         if(message.channel.type === 'dm') return;
+
+        let substringArray = get_substrings_between(message.content, ":", ":")
+        let msg = message.content;
+        if(!substringArray.length) return;
+
+        substringArray.forEach(m => {
+          let emoji = this.client.emojis.cache.find(x => x.name === m)
+          var replace = `:${m}:`;
+          var rexreplace = new RegExp(replace, 'g');
+
+          if(emoji && !msg.split(" ").find(x => x === emoji.toString()) && !msg.includes(`<a${replace}${emoji.id}>`)) msg = msg.replace(rexreplace, emoji.toString());
+        })
+
+        if(msg === message.content) return;
+
+        let webhook = await message.channel.fetchWebhooks();
+        webhook = webhook.find(x => x.name === "DolphinEmojis");
+
+        if(!webhook) {
+          webhook = await message.channel.createWebhook('DolphinEmojis', {
+            avatar: this.client.user.displayAvatarURL({ dynamic: false })
+          });
+        }
+
+        await webhook.edit({
+          name: message.member.nickname ? message.member.nickname : message.author.username,
+          avatar: message.author.displayAvatarURL({ dynamic: true })
+        })
+
+        message.delete().catch(m => {})
+
+        webhook.send(msg).catch(m => {});
+
+        await webhook.edit({
+          name: 'DolphinEmojis',
+          avatar: this.client.user.displayAvatarURL({ dynamic: false })
+        })
 
         const [commandName, ...args] = message.content.slice(this.client.prefix.length).trim().split(/ +/g);
 
@@ -81,3 +117,27 @@ module.exports = class extends Event {
         }
     }
 };
+
+    function get_substrings_between(str, startDelimiter, endDelimiter) {
+      var contents = [];
+      var startDelimiterLength = startDelimiter.length;
+      var endDelimiterLength = endDelimiter.length;
+      var startFrom = contentStart = contentEnd = 0;
+
+      while(false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
+        contentStart += startDelimiterLength;
+        contentEnd = strpos(str, endDelimiter, contentStart);
+        if(false === contentEnd) {
+          break;
+        }
+        contents.push(str.substr(contentStart, contentEnd - contentStart));
+        startFrom = contentEnd + endDelimiterLength;
+      }
+
+      return contents;
+    }
+
+    function strpos(haystack, needle, offset) {
+      var i = (haystack + '').indexOf(needle, (offset || 0));
+      return i === -1 ? false : i;
+    }
