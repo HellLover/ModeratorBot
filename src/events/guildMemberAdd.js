0c659a6ed56d00e2191bfa3f405c949c285971d9
HellLover <68247946+HellLover@users.noreply.github.com>
2020-11-17 11:03:03 +0400

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -19,16 +19,16 @@ module.exports = class extends Event {
     async execute(member) {
        if(member.guild.id !== "715290558779883532") return;
 
-        this.client.channels.cache.get("715306380357009558").send(`:flag_gb: | Welcome to Dolphin's support server, ${member.user}!\n\n:flag_ru: | Добро пожаловать на сервер поддержки бота Dolphin, ${member.user}!`)
+        member.guild.channels.cache.get("715306380357009558").send(`:flag_gb: | Welcome to Dolphin's support server, ${member.user}!\n\n:flag_ru: | Добро пожаловать на сервер поддержки бота Dolphin, ${member.user}!`)
 
         let role = member.guild.roles.cache.get("715298416199991346")
 
         await member.roles.add(role.id)
 
         if(member.guild.id !== stats.serverID) return;
-      this.client.channels.cache.get(stats.total).setName(`Member Count: ${member.guild.memberCount}`);
-      this.client.channels.cache.get(stats.channels).setName(`Channels: ${member.guild.channels.cache.size}`);
-      this.client.channels.cache.get(stats.roles).setName(`Roles: ${member.guild.roles.cache.size}`);
+      member.guild.channels.cache.get(stats.total).setName(`Member Count: ${member.guild.memberCount}`);
+      member.guild.channels.cache.get(stats.channels).setName(`Channels: ${member.guild.channels.cache.size}`);
+      member.guild.channels.cache.get(stats.roles).setName(`Roles: ${member.guild.roles.cache.size}`);
 
     }
 };
